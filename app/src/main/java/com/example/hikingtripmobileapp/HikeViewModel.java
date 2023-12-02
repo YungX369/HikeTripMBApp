8f4849e7e4851854c9878e66789394b3170ebbcf
YungX369 <94831363+YungX369@users.noreply.github.com>
2023-12-03 02:06:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hikingtripmobileapp/HikeViewModel.java
@@ -0,0 +1,26 @@
+package com.example.hikingtripmobileapp;
+
+import android.app.Application;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import java.util.List;
+
+public class HikeViewModel extends AndroidViewModel {
+
+    private HikeDbHelper hikeDbHelper;
+    private LiveData<List<Hike>> hikesLiveData;
+
+    public HikeViewModel(Application application) {
+        super(application);
+        hikeDbHelper = new HikeDbHelper(application);
+        hikesLiveData = hikeDbHelper.getAllHikesLiveData();
+    }
+
+    LiveData<List<Hike>> getHikesLiveData() {
+        return hikesLiveData;
+    }
+
+    public void insertHike(Hike hike) {
+        hikeDbHelper.addHike(hike);
+    }
+}
